880dee73c909e76b7021599d498cd4d5def974d6
peishi <peishi@qunhemail.com>
2020-01-03 17:49:51 +0800

    js,

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
-const Observable = require('./observable');
+const parentObj = {leakEdge: []};
 
-const observable = new Observable(function(observer) {
-  observer.next(1)
-  observer.next(2)
-  observer.next(3)
-  observer.complete()
-  observer.next(00)
-})
-
-var observer = {
-  next: function(value) {
-    console.log(value)
-  },
-  complete: function() {
-    console.log('complete!');
-  }
+function grow() {
+  parentObj.leakEdge.push(1);
 }
 
-observable.map(d => d + 1).subscribe(observer)
+document.getElementById('create').addEventListener('click', grow);
