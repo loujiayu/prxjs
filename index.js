1283a579448385eb0bcde07407ea92775305249a
peishi <peishi@qunhemail.com>
2020-01-07 10:40:59 +0800

    aa

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const parentObj = {leakEdge: []};
 
-function grow() {
-  parentObj.leakEdge.push(1);
+// function grow() {
+//   parentObj.leakEdge.push(1);
+//   const aa = [];
+//   aa.push(1);
+// }
+
+class Agent {
+  constructor(params) {
+    this.foo = params;
+    this.aa = []
+  }
+
+  grow() {
+    parentObj.leakEdge.push(1);
+    this.aa.push(1);
+  }
 }
 
-document.getElementById('create').addEventListener('click', grow);
+const agent = new Agent({aa: 32})
+
+document.getElementById('create').addEventListener('click', agent.grow.bind(agent));
