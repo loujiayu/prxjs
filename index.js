436e50af5ec23926b2f9bb5bd41561398a5ab8b4
peishi <peishi@qunhemail.com>
2020-01-08 11:18:25 +0800

    change

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-const parentObj = {leakEdge: []};
+var parentObj = {leakEdge: []};
 
-// function grow() {
-//   parentObj.leakEdge.push(1);
-//   const aa = [];
-//   aa.push(1);
-// }
-
-class Agent {
-  constructor(params) {
-    this.foo = params;
-    this.aa = []
-  }
-
-  grow() {
-    parentObj.leakEdge.push(1);
-    this.aa.push(1);
-  }
+function grow(arg1, arg2) {
+  parentObj.leakEdge.push(1);
+  console.log(arg1)
+  console.log(arg2)
+  // const aa = [];
+  // aa.push(1);
 }
 
-const agent = new Agent({aa: 32})
+// class Agent {
+//   constructor(params) {
+//     this.foo = params;
+//     this.aa = []
+//   }
 
-document.getElementById('create').addEventListener('click', agent.grow.bind(agent));
+//   grow() {
+//     parentObj.leakEdge.push(1);
+//     this.aa.push(1);
+//   }
+// }
+
+// const agent = new Agent({aa: 32})
+
+document.getElementById('create').addEventListener('click', grow.bind(this, 1,2));
