5c883739fd3d5bc5754ba269fc10765917fe5452
peishi <peishi@qunhemail.com>
2020-03-02 15:50:30 +0800

    change

--- index.js
+++ index.js
@@ -1,27 +1,18 @@
-// var obj = {};
-// var i = 0;
-// var power = 2;
-
-// function grow() {
-//   var top = Math.pow(2, power);
-//   power++;
-//   for (var j = 0; j < top; j++) {
-//     obj[Math.random()] = Math.random();
-//   }
-//   // Adds more properties, but properly deletes them.
-//   // Not a leak.
-//   var second = Math.random();
-//   obj[second] = second;
-
-// }
-
-// document.getElementById('create').addEventListener('click', grow.bind(this, 1,2));
-
-
 const parentObj = {leakEdge: []};
+
+class Test {
+  pobj = {leakEdge: []}
+  grow() {
+    this.pobj.leakEdge.push(new Array(1000000).join('x'));
+  }
+}
+
+const t = new Test()
 
 function grow() {
   parentObj.leakEdge.push(new Array(1000000).join('x'));
 }
 
-document.getElementById('create').addEventListener('click', grow);
+document.getElementById('create').addEventListener('click', () => {
+  t.grow();
+});
